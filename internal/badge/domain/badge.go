287c4d54f27421c0b7b4eea0c5e16cbf3b9c6b38
ciaabcdefg <ciaabcdefg@gmail.com>
2026-07-31 13:25:36 +0700

    feat: reorganize Badge struct fields for clarity

--- internal/badge/domain/badge.go
+++ internal/badge/domain/badge.go
@@ -3,6 +3,12 @@ package domain
 import "github.com/google/uuid"
 
 type Badge struct {
-	ID     uuid.UUID
-	Weight float64
+	ID       uuid.UUID
+	ImageURL string
+	Weight   float64
+}
+
+type SalakBadge struct {
+	SalakID uuid.UUID
+	BadgeID uuid.UUID
 }
